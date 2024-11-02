6bc3d06279299da16a177be8f8a90a6e68969550
Robin Stumpf <rstumpf@1-C-24.42heilbronn.de>
2024-11-02 13:37:48 +0100

    reading line and stop with /n but still to much characters

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,42 @@
 /*   By: rstumpf <rstumpf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:50:18 by rstumpf           #+#    #+#             */
-/*   Updated: 2024/10/29 16:26:30 by rstumpf          ###   ########.fr       */
+/*   Updated: 2024/11/02 13:36:31 by rstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// char	*get_next_line(int fd)
-// {
-	
-// }
+// 1st allocate enough Space for the first read
+// 2nd safe bytes read and start reading into find_line
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*temp;
+	char		find_line[BUFFER_SIZE + 1];
+	ssize_t		bytes_read;
+
+	buffer = "";
+	while (buffer)
+	{
+		bytes_read = read(fd, find_line, BUFFER_SIZE);
+		find_line[bytes_read] = '\0';
+		temp = ft_strjoin(buffer, find_line);
+		buffer = temp;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (buffer);
+}
 
 int	main(void)
 {
 	int		fd;
-	char	buffer[100];
-	ssize_t	bytes_read;
+	char	*buffer;
 
 	fd = open("my_poem.txt", O_RDONLY);
-	bytes_read = read(fd, buffer, 3);
-	buffer[bytes_read] = '\0';
+	buffer = get_next_line(fd);
 	printf("%s", buffer);
 	close(fd);
 	return (0);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,62 @@
 /*   By: rstumpf <rstumpf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:56:39 by rstumpf           #+#    #+#             */
-/*   Updated: 2024/10/28 17:56:40 by rstumpf          ###   ########.fr       */
+/*   Updated: 2024/10/29 17:59:38 by rstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if (*s == (char) c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	if ((char) c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*p;
+	int		i;
+	int		j;
+
+	p = malloc(ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1);
+	if (!p)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		p[i] = s2[j];
+		j++;
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+size_t	ft_strlen(const char *string)
+{
+	int	i;
+
+	i = 0;
+	while (*string != '\0')
+	{
+		string++;
+		i++;
+	}
+	return (i);
+}
