b8dfffd3de49c78a1901e493bd8fde389c406adf
Robin Stumpf <rstumpf@1-B-27.42heilbronn.de>
2024-11-05 15:17:21 +0100

    i cant find the leaks by my own, only with paco :/

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rstumpf <rstumpf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:50:18 by rstumpf           #+#    #+#             */
-/*   Updated: 2024/11/03 16:09:10 by rstumpf          ###   ########.fr       */
+/*   Updated: 2024/11/05 15:15:43 by rstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ char	*get_next_line(int fd)
 	{
 		bytes_read = read(fd, find_line, BUFFER_SIZE);
 		if (bytes_read == -1)
-			return (NULL);
+			return (buffer = NULL,  NULL);
 		if (bytes_read == 0)
 		{
 			if (buffer && *buffer)
@@ -44,49 +44,49 @@ char	*get_next_line(int fd)
 		find_line[bytes_read] = '\0';
 		buffer = ft_strjoin(buffer, find_line);
 		if (!buffer)
-			return (NULL);
+			return (free(buffer), NULL);
 	}
 	output = ft_createline(buffer);
 	buffer = ft_getremainder(buffer);
 	return (output);
 }
 
-// int	main(void)
-// {
-// 	// int fd;
+int	main(void)
+{
+	// int fd;
 	
-// 	// fd = open("my_poem.txt", O_RDONLY);
-// 	// char	*buffer1;
-// 	// char	*buffer2;
-// 	// char	*buffer3;
-// 	// char	*buffer4;
+	// fd = open("my_poem.txt", O_RDONLY);
+	// char	*buffer1;
+	// char	*buffer2;
+	// char	*buffer3;
+	// char	*buffer4;
 
-// 	// buffer1 = get_next_line(fd);
-// 	// buffer2 = get_next_line(fd);
-// 	// buffer3 = get_next_line(fd);
-// 	// buffer4 = get_next_line(fd);
-// 	// printf("%s", buffer1);
-// 	// printf("%s", buffer2);
-// 	// printf("%s", buffer3);
-// 	// printf("%s", buffer4);
-// 	// close(fd);
+	// buffer1 = get_next_line(fd);
+	// buffer2 = get_next_line(fd);
+	// buffer3 = get_next_line(fd);
+	// buffer4 = get_next_line(fd);
+	// printf("%s", buffer1);
+	// printf("%s", buffer2);
+	// printf("%s", buffer3);
+	// printf("%s", buffer4);
+	// close(fd);
 	
-// 	int		fd;
-// 	char	*line;
+	int		fd;
+	char	*line;
 
-// 	fd = open("my_poem.txt", O_RDONLY);
-// 	if (fd < 0)
-// 	{
-// 		printf("open file failed\n");
-// 		exit(1);
-// 	}
-// 	while (1)
-// 	{
-// 		line = get_next_line(fd);
-// 		if (!line)
-// 			break ;
-// 		printf("%s", line);
-// 	}
-// 	close(fd);
-// 	return (0);
-// }
+	fd = open("my_poem.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("open file failed\n");
+		exit(1);
+	}
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		printf("%s", line);
+	}
+	close(fd);
+	return (0);
+}
