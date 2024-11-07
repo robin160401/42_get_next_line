8e13359b7430b26d9783647b7ba543ebea05c6f7
Robin Stumpf <rstumpf@1-A-30.42heilbronn.de>
2024-11-07 10:53:40 +0100

    logic seems working

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rstumpf <rstumpf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:50:18 by rstumpf           #+#    #+#             */
-/*   Updated: 2024/11/06 20:51:39 by rstumpf          ###   ########.fr       */
+/*   Updated: 2024/11/07 10:53:22 by rstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,32 @@
 
 char	*get_next_line(int fd)
 {
-	static char	leftover[BUFFER_SIZE + 3];
-	char		buffer[BUFFER_SIZE + 1];
+	static char	leftover[BUFFER_SIZE + 1];
 	ssize_t		bytes_read;
-	char		*output = NULL;
+	char		*output;
 	int			i;
 
+	output = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!output)
 	{
 		output = malloc(1 * sizeof(char));
+		if (!output)
+			return (NULL);
 		output[0] = '\0';
 	}
 	output = ft_strjoin(output, leftover);
-
-	while (!ft_strchr(buffer, '\n'))
+	if (!output)
+		return (NULL);
+	while (!ft_strchr(output, '\n'))
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		bytes_read = read(fd, leftover, BUFFER_SIZE);
 		if (bytes_read == 0)
 		{
 			if (leftover[0] == 0)
 				return (NULL);
-			ft_bzero(leftover, BUFFER_SIZE + 3);
+			ft_bzero(leftover, BUFFER_SIZE + 1);
 			i = 0;
 			while (output[bytes_read] != '\n' && output[bytes_read])
 				bytes_read++;
@@ -50,61 +53,67 @@ char	*get_next_line(int fd)
 				bytes_read++;
 			}
 			output = ft_createline(output);
+			if (!output)
+				return (NULL);
 			return (output);
 		}
-		buffer[bytes_read] = '\0';
-		output = ft_strjoin(output, buffer);
+		leftover[bytes_read] = '\0';
+		output = ft_strjoin(output, leftover);
+		if (!output)
+			return (NULL);
 	}
 	output = ft_createline(output);
+	if (!output)
+		return (NULL);
 	bytes_read = 0;
 	i = 0;
-	while (buffer[bytes_read] != '\n' && buffer[bytes_read])
+	while (leftover[bytes_read] != '\n' && leftover[bytes_read])
 		bytes_read++;
-	while (buffer[bytes_read])
+	while (leftover[bytes_read])
 	{
-		leftover[i] = buffer[bytes_read + 1];
+		leftover[i] = leftover[bytes_read + 1];
 		i++;
 		bytes_read++;
 	}
 	return (output);
 }
 
-int	main(void)
-{
-	// int fd;
+// int	main(void)
+// {
+// 	// int fd;
 	
-	// fd = open("my_poem.txt", O_RDONLY);
-	// char	*buffer1;
-	// char	*buffer2;
-	// char	*buffer3;
-	// char	*buffer4;
+// 	// fd = open("my_poem.txt", O_RDONLY);
+// 	// char	*buffer1;
+// 	// char	*buffer2;
+// 	// char	*buffer3;
+// 	// char	*buffer4;
 
-	// buffer1 = get_next_line(fd);
-	// buffer2 = get_next_line(fd);
-	// buffer3 = get_next_line(fd);
-	// buffer4 = get_next_line(fd);
-	// printf("%s", buffer1);
-	// printf("%s", buffer2);
-	// printf("%s", buffer3);
-	// printf("%s", buffer4);
-	// close(fd);
+// 	// buffer1 = get_next_line(fd);
+// 	// buffer2 = get_next_line(fd);
+// 	// buffer3 = get_next_line(fd);
+// 	// buffer4 = get_next_line(fd);
+// 	// printf("%s", buffer1);
+// 	// printf("%s", buffer2);
+// 	// printf("%s", buffer3);
+// 	// printf("%s", buffer4);
+// 	// close(fd);
 	
-	int		fd;
-	char	*line;
+// 	int		fd;
+// 	char	*line;
 
-	fd = open("my_poem.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		printf("open file failed\n");
-		exit(1);
-	}
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		printf("%s", line);
-	}
-	close(fd);
-	return (0);
-}
+// 	fd = open("my_poem.txt", O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		printf("open file failed\n");
+// 		exit(1);
+// 	}
+// 	while (1)
+// 	{
+// 		line = get_next_line(fd);
+// 		if (!line)
+// 			break ;
+// 		printf("%s", line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rstumpf <rstumpf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:50:14 by rstumpf           #+#    #+#             */
-/*   Updated: 2024/11/06 20:45:12 by rstumpf          ###   ########.fr       */
+/*   Updated: 2024/11/07 10:47:50 by rstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,5 @@ char	*ft_createline(char *string);
 char	*ft_getremainder(char *string);
 char	*ft_strdup(const char *s1);
 void	ft_bzero(void *s, size_t n);
-
 
 #endif
